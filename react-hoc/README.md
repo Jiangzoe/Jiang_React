6bd454f47c05421a80970829c17ac2e33a7a8400
Jiangzoe <1097288257@qq.com>
2019-06-24 15:30:19 +0800

    react-hoc

---
+++ react-hoc/README.md
@@ -0,0 +1,8 @@
+# hoc (high order component)
+接受一个函数作为组件 返回一个组件  
+
+## 组件
+
+
+## @
+ES7 里面的 装饰器decorater
---
+++ react-hoc/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ShopCart from './ShopCart'
+import LoginStatus from './LoginStatus'
+import WithLogin from './WithLogin'
+import './App.css';
+
+const WithLoginStatus = WithLogin(LoginStatus)
+const WithShopCart = WithLogin(ShopCart)
+function App() {
+  return (
+    // <React.Fragment>
+    <>
+      {/* <WithLoginStatus></WithLoginStatus>
+      <WithShopCart a="1" b="2"></WithShopCart> */}
+      <LoginStatus></LoginStatus>
+      <ShopCart></ShopCart>
+    </>
+    // </React.Fragment>
+  );
+}
+
+export default App;
