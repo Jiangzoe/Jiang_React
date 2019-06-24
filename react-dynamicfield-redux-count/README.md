65546a0dad8cc2436ae9eb3db44688813563302b
Jiangzoe <1097288257@qq.com>
2019-06-24 09:48:16 +0800

    react-redux

---
+++ react-dynamicfield-redux-count/README.md
@@ -0,0 +1,11 @@
+# Redux
+## action
+本质是js的一个对象  
+干什么 type:' INCREMENT'  
+传递数据  
+
+## Reducer
+本质是一个函数 更新 store
+
+## store
+存数据的地方
---
+++ react-dynamicfield-redux-count/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'antd/dist/antd.css'
+import './App.css';
+import DynamicFiled from './DynamicFiled'
+
+function App() {
+  return (
+    <div>
+      <DynamicFiled></DynamicFiled>
+    </div>
+  );
+}
+
+export default App;
