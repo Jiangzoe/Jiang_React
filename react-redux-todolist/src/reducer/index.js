a32684c851bd59ceeddcaac3aa8f92c11ca6517d
Jiangzoe <1097288257@qq.com>
2019-06-27 09:55:52 +0800

    react-redux

---
+++ react-redux-todolist/src/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import todos from './todos'
+import filterReducer from './filter'
+
+
+export default combineReducers({
+  todos,
+  filterType:filterReducer
+})
