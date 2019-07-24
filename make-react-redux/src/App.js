37effa463e32c2170e081b2eab3ffbe27867cb79
Jiangzoe <1097288257@qq.com>
2019-07-24 09:57:29 +0800

    react-redux

---
+++ make-react-redux/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ContextDemo from './contextDemo'
+import Count from './count'
+
+function App() {
+  return (
+    <div className="App">
+      <ContextDemo></ContextDemo>
+      <Count></Count>
+    </div>
+  );
+}
+
+export default App;
