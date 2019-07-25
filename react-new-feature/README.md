af808e4812671f9bb8735041304d4f0f75cfc65b
Jiangzoe <1097288257@qq.com>
2019-07-25 09:51:31 +0800

    Concurrent mode

---
+++ react-new-feature/README.md
@@ -0,0 +1,3 @@
+# React16新特性
+## Concurrent mode
+让react的整体渲染有一个优先级的排比
---
+++ react-new-feature/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ConcurrentModeDemo from './ConcurrentMode/index'
+
+function App() {
+  return (
+    <div className="App">
+      <ConcurrentModeDemo></ConcurrentModeDemo>
+    </div>
+  );
+}
+
+export default App;
